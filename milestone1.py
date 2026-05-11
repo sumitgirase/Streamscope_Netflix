c057adbe6c7f2443336d565b770e053e902f052a
Sumit Girase <sumitgirase9162@gmail.com>
2026-05-11 22:55:24 +0530

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -1,19 +1,19 @@
 import pandas as pd
 df = pd.read_csv("netflix_titles.csv")
 
-#print(df.head())
+print(df.head())
 
-#print(df.info())
+print(df.info())
 
-#print(df.isnull().sum()) #it checks missing values in each columns
+print(df.isnull().sum()) #it checks missing values in each columns
 
-# for removing duplicates
-#df.drop_duplicates(inplace=True)
-#df['director'] = df['director'].fillna("Unknown")
-#df['country'] = df['country'].fillna("Unknown")
+for removing duplicates
+df.drop_duplicates(inplace=True)
+df['director'] = df['director'].fillna("Unknown")
+df['country'] = df['country'].fillna("Unknown")
 
-#df['country'] = df['country'].str.strip().str.title()
-#df['rating'] = df['rating'].str.strip()
+df['country'] = df['country'].str.strip().str.title()
+df['rating'] = df['rating'].str.strip()
 
 
 df.to_csv("netflix_cleaned.csv", index=False)
